747bf9645385a6a5dd8c040a141ed6d1d90363ab
fliblip <wickynjy@gmail.com>
2022-09-02 16:06:39 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    updateToDo(e.target.input_new_task.value)
+
+  })
 });
+
+function updateToDo(todo){
+  // let tasklist = document.querySelector('#tasks')
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  
+  p.appendChild(btn)
+  // document.querySelector('#main-content').appendChild('btn')
+  btn.addEventListener('click', handleDelete)
+  document.querySelector('#tasks').appendChild(p)
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
+
+
+
